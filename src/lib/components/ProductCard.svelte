d085b4f240d3d3e5528a1a0f0364422def0145ca
Jesus Berdugo <berdugopertuz@gmail.com>
2024-10-18 08:21:20 -0500

    feat: delete product

--- src/lib/components/ProductCard.svelte
+++ src/lib/components/ProductCard.svelte
@@ -2,17 +2,46 @@
   import * as Card from "$lib/components/ui/card";
   import type { Product } from "../../schemas/product";
   import Button from "./ui/button/button.svelte";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+  import Ellipsis from "lucide-svelte/icons/ellipsis";
+  import { createEventDispatcher } from "svelte";
+  import { HttpClient } from "../../services/http";
   
   export let product: Product;
+  
+  const http = HttpClient.getInstance();
+  const dispatch = createEventDispatcher();
+  
+  async function handleDelete() {
+    await http.delete(`/product/${product.id}`)
+    dispatch('delete', product)
+  }
 </script>
 
 <Card.Root>
   <Card.Content>
+    <div class="flex justify-end mb-4">
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Ellipsis />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Group>
+            <DropdownMenu.Label>Menu</DropdownMenu.Label>
+            <DropdownMenu.Separator />
+            <DropdownMenu.Item>Edit</DropdownMenu.Item>
+            <DropdownMenu.Item on:click={handleDelete}>Delete</DropdownMenu.Item>
+          </DropdownMenu.Group>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </div>
+    
     <img
-        src={product.imageUrl}
-        alt={product.name}
-        class="h-[200px] w-full object-cover object-center rounded-md"
-      />
+      src={product.imageUrl}
+      alt={product.name}
+      class="h-[200px] w-full object-cover object-center rounded-md"
+    />
+    
     <div class="flex flex-col gap-2 mt-4">
       <Card.Title>{product.name}</Card.Title>
       <Card.Description>{product.description}</Card.Description>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -15,6 +15,10 @@
   function handleAddNewProduct({ detail }: { detail: Product }) {
     data.products.push(detail)
   }
+
+  function handleDeleteProduct({ detail }: { detail: Product }) {
+    data.products = data.products.filter((product) => product.id !== detail.id)
+  }
 </script>
 
 <svelte:head>
@@ -34,7 +38,7 @@
 <div class="grid grid-cols-1 gap-4 px-2 justify-center lg:grid-cols-4">
   {#if data.products.length}
     {#each data.products as product}
-      <ProductCard {product} />
+      <ProductCard {product} on:delete={handleDeleteProduct} />
     {/each}
   {:else}
     <span>No hay productos</span>
